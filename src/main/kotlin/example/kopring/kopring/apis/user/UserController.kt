48b10bc7c4b9d064c65569637ea79d834b369986
gangyeolkim <kkyy3402@naver.com>
2023-03-04 23:03:55 +0900

    fix: controller 리턴 타입 변경

--- src/main/kotlin/example/kopring/kopring/apis/user/UserController.kt
+++ src/main/kotlin/example/kopring/kopring/apis/user/UserController.kt
@@ -2,6 +2,7 @@ package example.kopring.kopring.apis.user
 
 import example.kopring.kopring.apis.user.dtos.CreateUserRequestDto
 import example.kopring.kopring.apis.user.entities.UserEntity
+import example.kopring.kopring.common.response.ApiResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -11,20 +12,20 @@ import org.springframework.web.bind.annotation.*
 class UserController(private val userService: UserService) {
 
     @GetMapping("/{userId}")
-    fun fetchUser(@PathVariable userId: Long): ResponseEntity<Any> {
+    fun fetchUser(@PathVariable userId: Long): ResponseEntity<ApiResponse<Any>> {
         val user = userService.getUserById(userId)
-        return ResponseEntity.ok(user)
+        return ApiResponse.success("data", user)
     }
 
     @GetMapping("")
-    fun fetchAllUsers(): ResponseEntity<Any> {
+    fun fetchAllUsers(): ResponseEntity<ApiResponse<Any>> {
         val users = userService.fetchAllUser()
-        return ResponseEntity.ok(users)
+        return ApiResponse.success("data", users)
     }
 
     @PostMapping("")
-    fun createUser(@RequestBody createUserRequestDto: CreateUserRequestDto): ResponseEntity<Any> {
+    fun createUser(@RequestBody createUserRequestDto: CreateUserRequestDto): ResponseEntity<ApiResponse<Any>> {
         val createdUser = userService.createUser(createUserRequestDto)
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser)
+        return ApiResponse.success("data", createdUser)
     }
 }
